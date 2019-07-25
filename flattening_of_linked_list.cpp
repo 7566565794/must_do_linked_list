82ed011a32e0dc5f679d847a18237422ea57ccfd
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-07-25 10:09:56 +0530

    Update flattening_of_linked_list.cpp

--- flattening_of_linked_list.cpp
+++ flattening_of_linked_list.cpp
@@ -34,3 +34,41 @@ Node *flatten(Node *root)
 //   cout<<endl;
    return NULL;
 }
+
+
+//Another method to flatten a linked list
+
+
+/* Node structure  used in the program
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom ;
+}; */
+/*  Function which returns the  root of 
+    the flattened linked list. */
+    
+Node *merge(Node *x, Node *y){
+    if(!x)    
+        return y;
+    if(!y)
+        return x;
+    Node *result = NULL;
+    if(x->data<y->data){
+        result = x;
+        result->bottom = merge(x->bottom,y);
+    }
+    else{
+        result = y;
+        result->bottom = merge(x,y->bottom);
+    }
+    return result;
+}
+
+Node *flatten(Node *root)
+{
+   // Your code here
+   if(!root || !root->next)
+        return root;
+    return merge(root, flatten(root->next));
+}
